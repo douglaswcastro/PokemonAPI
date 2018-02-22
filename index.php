9631a1b9e8e049dbf3509a5404b3e91d4344ce3d
Douglas Willian <douglaswcastro@hotmail.com>
2018-02-22 00:00:18 -0300

    COMMIT

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<!DOCTYPE>
-<html>
+<!DOCTYPE html>
+<html ng-app="meuModulo">
 	<head>
 		<title>Pokemon</title>
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 		<script src="lib/angular.min.js"></script>
-		<script type="text/javascript"></script>
 		<script src="controllers/app.js"></script>
 		
 	</head>
-	<body ng-app="meuModulo">
-		<div ng-controller="IndexController">
+	<body>
+		<div class="jumbotron" ng-controller="IndexController">
 			{{titulo}}
 		</div>
 	
